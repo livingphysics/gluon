5c7575b2675b1d08b69586e3b4e22c533b319e87
davex0r <dave.xor@gmail.com>
2025-11-24 15:57:53 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 """
-Utility functions for bioreactor operations. Test
+Utility functions for bioreactor operations. These are not intended to be used directly by the user, but rather to be used by the bioreactor class.
 These functions are designed to be used with bioreactor.run() for scheduled tasks.
 """
 
